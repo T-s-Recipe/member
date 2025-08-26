738c0155e06733043eca81676fe9229c6ad67b5c
jonghyukleee <jh5253krkr@naver.com>
2025-08-27 02:42:53 +0900

    modify: `@MemberId`
    - hidden 옵션 추가

--- src/main/kotlin/shop/tsrecipe/member/annotation/MemberId.kt
+++ src/main/kotlin/shop/tsrecipe/member/annotation/MemberId.kt
@@ -1,5 +1,8 @@
 package shop.tsrecipe.member.annotation
 
+import io.swagger.v3.oas.annotations.Parameter
+
+@Parameter(hidden = true)
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class MemberId
